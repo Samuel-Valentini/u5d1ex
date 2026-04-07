f562c0632a1eab24e7bd5d339b25b826fb3e6ad0
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-07 15:30:03 +0200

    Initial commit

---
+++ src/main/java/samuelvalentini/u5d1ex/U5d1exApplication.java
@@ -0,0 +1,13 @@
+package samuelvalentini.u5d1ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d1exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d1exApplication.class, args);
+	}
+
+}
